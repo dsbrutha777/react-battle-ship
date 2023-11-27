73ab6d322c2702489fc093f95696a036cbe3c6ce
andy.hx.lee <andy.hx.lee@ibm.com>
2023-11-27 15:46:22 +0800

    first commit

---
+++ src/components/ui/mode-toggle.tsx
@@ -0,0 +1,41 @@
+// 也可以換成任何喜歡的 icon 包
+import { Moon, Sun } from "lucide-react"
+import { Button } from "@/components/ui/button"
+
+// 預設的 mode toggle 元件是有下拉選單的
+// 因此會需要載入 dropdown-menu 元件，不過可依照需求修改
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+// 載入剛剛在 theme-provider 中設定的 custom hook
+import { useTheme } from "@/components/theme-provider"
+
+export function ModeToggle() {
+  const { setTheme } = useTheme()
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}

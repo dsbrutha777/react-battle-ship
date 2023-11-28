0f4cb83036855f05b838f94985032dec30acf082
andy.hx.lee <andy.hx.lee@ibm.com>
2023-11-29 00:03:40 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import { ModeToggle } from '@/components/ui/mode-toggle'
 import LandingPage from '@/components/pages/landing-page'
 
 function App() {
-
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="flex flex-col p-3 h-full">
-        <div className="self-end h-auto">
-          <ModeToggle />
+          <div className="self-end h-auto">
+            <ModeToggle />
+          </div>
+          <LandingPage />
         </div>
-        <LandingPage />
-      </div>
     </ThemeProvider>
   )
 }
--- src/components/pages/landing-page.tsx
+++ src/components/pages/landing-page.tsx
@@ -1,5 +1,7 @@
+/// <reference types="vite-plugin-svgr/client" />
 import { Button } from '@/components/ui/button'
-import BattleShip from '@/assets/battleship.svg'
+import BattleShip from '@/assets/battleship.svg?react'
+import Wave from '@/assets/wave.svg?react'
 
 function LandingPage() {
 
@@ -9,13 +11,18 @@ function LandingPage() {
         <h1 className="text-4xl">Battle Ship</h1>
       </header>
 
-      <main className="flex flex-col justify-center items-center">
-        <img src={BattleShip} alt="battleship" className="max-h-96" />
+      <main className="flex flex-row justify-center items-center">
+        <Wave className="max-h-96" />
+        <BattleShip className="max-h-96" />
+        <Wave className="max-h-96" />
       </main>
 
-      <footer className="flex flex-col justify-center items-center">
-        <Button>footer</Button>
+      <footer className="flex flex-row justify-center items-center gap-x-3 relative">
+        <Button>CREATE</Button>
+        <Button>SEARCH</Button>
       </footer>
+
+      
     </div>
     )
 }

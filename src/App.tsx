c755b99abfed9ec7a9c43354a671ff515436a414
andy.hx.lee <andy.hx.lee@ibm.com>
2023-11-30 18:20:19 +0800

    integrate firebase to deploy & hosting

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,25 @@ import Wave from '@/assets/wave.svg?react'
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button'
 
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB2mMKqqvId7PvMizr1olwm0wy7MJluNPA",
+//   authDomain: "react-battle-ship.firebaseapp.com",
+//   projectId: "react-battle-ship",
+//   storageBucket: "react-battle-ship.appspot.com",
+//   messagingSenderId: "862770292919",
+//   appId: "1:862770292919:web:5cc5531a54815c5c5e4532"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+
 function App() {
   const navigate = useNavigate();
   const handleCreateRoomClick = () => {

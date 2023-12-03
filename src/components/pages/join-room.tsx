7899c5713d3829d360b47e9fa4203600860e462e
Andy Lee <andylee@lihongxundeMacBook-Pro.local>
2023-12-03 18:17:57 +0800

    remove redundant code

--- src/components/pages/join-room.tsx
+++ src/components/pages/join-room.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useMemo, useEffect } from 'react'
+import { useState, useCallback, useMemo } from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { initializeApp } from "firebase/app";

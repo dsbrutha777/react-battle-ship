184182b0b8ec848d307a8a79f2125d709a50e857
andy.hx.lee <andy.hx.lee@ibm.com>
2023-12-05 12:29:03 +0800

    remove redundant code

--- src/components/ui/top-bar.tsx
+++ src/components/ui/top-bar.tsx
@@ -1,37 +1,20 @@
 import { useCallback, useMemo } from "react"
-import { useNavigate, useLocation, useParams } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { ReactNode } from "react";
 import { ArrowLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, remove } from "firebase/database";
-import { firebaseConfig } from "@/firebase-config";
 
 interface TopBarProps {
     children: ReactNode;
 }
 
-const app = initializeApp(firebaseConfig);
-
 export function TopBar({ children }: TopBarProps) {
-    const db = useMemo(() => getDatabase(app), []);
-    const params = useParams();
     const location = useLocation();
     const navigate = useNavigate();
     const visibileClassName = useMemo(() => location.pathname === '/' ? 'invisible' : 'visible', [location.pathname]);
     const handleBackClick = useCallback(async () => {
-        const currentRoute = location.pathname.split('/')[1]; // create-room
-        switch(currentRoute) {
-            case 'create-room':
-                const { roomId } = params;
-                const roomsRef = ref(db, `rooms/${roomId}`);
-                await remove(roomsRef);
-                break;
-            default: break;
-        }
-
         navigate(-1);
     }, [location.pathname, navigate]);
     return (
